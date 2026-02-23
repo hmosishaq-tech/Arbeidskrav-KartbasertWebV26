ca9ee59d6c540f9c40ab4932a8602b7a2f817fa4
Hmosi <hmosishaq@gmail.com>
2026-02-23 16:03:42 +0100

    Commit 23.02

--- src/src/components/layers/tilfluktsromLayer.tsx
+++ src/src/components/layers/tilfluktsromLayer.tsx
@@ -3,16 +3,32 @@ import VectorSource from "ol/source/Vector.js";
 import { GeoJSON } from "ol/format.js";
 import { Circle, Fill, Stroke, Style, Text } from "ol/style.js";
 
+import type { FeatureLike } from "ol/Feature";
+
+
+interface tilfluktsromProperties {
+    plasser: number;
+    romnr: number;
+    adresse: String;
+}
+
+function tilfluktsStyle(feature: FeatureLike) {
+    console.log(feature);
+
+    const props = feature.getProperties() as tilfluktsromProperties;
+
+    return new Style({
+        image: new Circle({
+            radius: 3 + props.plasser/1000,
+            stroke: new Stroke({width: 2, color: "black"}),
+            fill: new Fill({ color: "red" }),
+        }),
+    })
+}
 export const tilfluktsromLayer = new VectorLayer({
     source: new VectorSource({
-        url: "/Arbeidskrav-KartbasertWebV26/tilfluktsrom.geojson",
+        url: "/tilfluktsrom.geojson",
         format: new GeoJSON(),
     }),
-    style: new Style({
-        image: new Circle({
-            radius: 4,
-        stroke: new Stroke({width: 2, color: "white"}),
-        fill: new Fill({ color: "blue" }),
-        }),
-    }),
+    style: tilfluktsStyle,
 });
